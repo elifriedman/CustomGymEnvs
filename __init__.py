d8bb43049e35cb3f176b86741a93879bf1ac2b10
Eli Friedman <friedm3@cooper.edu>
2018-05-13 22:21:50 +0300

    change module

--- __init__.py
+++ __init__.py
@@ -5,17 +5,17 @@ from .nd_slider import NDSlider
 
 register(
     id='OneDSlider-v0',
-    entry_point='custom_envs:OneDSlider',
-    max_episode_steps=208,
+    entry_point='CustomGymEnvs:OneDSlider',
+    max_episode_steps=2048,
     kwargs={
-        "her": False,
+        "her": True,
     },
 )
 
 register(
     id='NDSlider-v0',
-    entry_point='custom_envs:NDSlider',
-    max_episode_steps=208,
+    entry_point='CustomGymEnvs:NDSlider',
+    max_episode_steps=2048,
     kwargs={
         "her": True,
         "N": 3,
