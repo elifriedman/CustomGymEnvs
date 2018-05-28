855556f6a6b24c693201c9e9746167bd4cf98e05
Eli Friedman <friedm3@cooper.edu>
2018-05-28 16:48:32 +0300

    change registration method

--- __init__.py
+++ __init__.py
@@ -1,27 +1,28 @@
-from gym.envs.registration import register
+from gym.envs.registration import register as _register
 
 from .oned_slider import OneDSlider
 from .nd_slider import NDSlider
 
-register(
-    id='OneDSlider-v0',
-    entry_point='CustomGymEnvs:OneDSlider',
-    max_episode_steps=2048,
-    kwargs={
-        "her": True,
-        "goal_sample": "pos", # options are 'zero', 'pos', 'vel'
-        "weight_sample": "rand", # options are "rand", "const"
-    },
-)
+def register(env, max_episode_steps=2048, **kwargs):
+    _register(
+        id='{}-v0'.format(env),
+        entry_point='CustomGymEnvs:{}'.format(env),
+        max_episode_steps=max_episode_steps,
+        kwargs=kwargs
+    )
 
-register(
-    id='NDSlider-v0',
-    entry_point='CustomGymEnvs:NDSlider',
-    max_episode_steps=2048,
-    kwargs={
-        "her": True,
-        "N": 3,
-        "goal_sample": "pos", # options are 'zero', 'pos', 'vel'
-        "weight_sample": "rand", # options are "rand", "const"
-    },
-)
+def register_all():
+    register("NDSlider", max_episode_steps=2048, her=True, N=3, goal_sample='zero', weight_sample='rand')
+    register("OneDSlider", max_episode_steps=2048, her=True, goal_sample='zero', weight_sample='rand')
+
+# register(
+#     id='NDSlider-v0',
+#     entry_point='CustomGymEnvs:NDSlider',
+#     max_episode_steps=2048,
+#     kwargs={
+#         "her": True,
+#         "N": 3,
+#         "goal_sample": "zero", # options are 'zero', 'pos', 'vel'
+#         "weight_sample": "rand", # options are "rand", "const"
+#     },
+# )
