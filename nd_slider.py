dba20e6805a8a7791b26555c8953a03e4da221c3
Eli Friedman <friedm3@cooper.edu>
2018-08-21 14:10:05 +0300

    remove render. add dt

--- nd_slider.py
+++ nd_slider.py
@@ -32,7 +32,7 @@ class NDSlider(gym.Env):
         'video.frames_per_second': 30
     }
 
-    def __init__(self, her=True, N=2, goal_sample='pos', weight_sample='rand'):
+    def __init__(self, her=True, N=2, goal_sample='pos', weight_sample='rand', dt=0.25)):
         self.min_action = -1.0
         self.max_action = 1.0
         self.min_position = -100.
@@ -40,6 +40,7 @@ class NDSlider(gym.Env):
         self.max_speed = 20.
         self.N = N
         self.her = her
+        self.dt = dt
 
         self.goal = None  # initialized in self.reset()
         self.weights = None  # initialized in self.reset()
@@ -48,17 +49,13 @@ class NDSlider(gym.Env):
         self.WEIGHT_SAMPLE_STRATEGY = weight_sample  # options are 'const', 'rand'
         if 'grid' in weight_sample:
             weight_density = int(weight_sample.split("_")[1])
-            self._init_weights(weight_density)
+            self._init_weight_grid(weight_density)
             self.WEIGHT_SAMPLE_STRATEGY = 'grid'
 
-        self.prev_render_info = None
-        self.curr_render_info = None
         self.t = 0
 
         self.low_state = np.array([self.min_position]*N + [-self.max_speed]*N)
         self.high_state = np.array([self.max_position]*N + [self.max_speed]*N)
-
-        self.viewer = None
 
         self.action_space = spaces.Box(low=self.min_action, high=self.max_action, shape=(N,), dtype="float32")
 
@@ -80,16 +77,14 @@ class NDSlider(gym.Env):
 
     def step(self, action):
 
-        self.prev_render_info = self.curr_render_info
-
         position, velocity = self.state[:self.N], self.state[self.N:]
         action = np.clip(action, self.action_space.low, self.action_space.high)
         s = np.abs(action).sum()
         action = action / s if s > 1 else action
 
-        velocity += action
+        velocity += action * self.dt
         velocity = np.clip(velocity, -self.max_speed, self.max_speed)
-        position += velocity
+        position += velocity * self.dt
         position = np.clip(position, self.min_position, self.max_position)
         velocity[(position==self.min_position)*(velocity<0)] = 0
         velocity[(position==self.max_position)*(velocity>0)] = 0
@@ -113,7 +108,6 @@ class NDSlider(gym.Env):
             'vector_reward': vector_reward,
         })
 
-        self.curr_render_info = [position/2, velocity*5, action*25]
         self.t += 1
         if self.her:
             return obs, reward, done, info
@@ -161,7 +155,7 @@ class NDSlider(gym.Env):
         else:
             return self.np_random.uniform(low=self.low_state, high=self.high_state)
 
-    def _init_weights(self, weight_density=3):
+    def _init_weight_grid(self, weight_density=3):
         weights = np.meshgrid(*[np.linspace(0, 1, weight_density)]*(2*self.N - 1))
         weights = np.array([w.reshape(-1) for w in weights]).T
         weights = weights[np.sum(weights, axis=1) <= 1]
@@ -195,75 +189,11 @@ class NDSlider(gym.Env):
         self.weights = self.sample_weights()
         self.state = np.concatenate([self.np_random.uniform(low=self.low_state[0:self.N], high=self.high_state[:self.N]),
                                      np.zeros(self.N)])
-        self.prev_render_info = None
-        self.curr_render_info = None
-        if self.viewer:
-            self.clear_render()
         self.t = 0
         return self._make_obs()
 
     def render(self, mode='human'):
-        screen_width = 600
-        screen_height = 400
-
-        world_width = self.max_position - self.min_position
-        scaler = screen_width/world_width
-
-        COLORS = np.eye(3)
-        upscale = 150
-
-        if self.viewer is None:
-            self.lines = []
-            carwidth = 20
-            carheight = 75
-            from gym.envs.classic_control import rendering
-            self.viewer = rendering.Viewer(screen_width, screen_height)
-            xs = np.linspace(self.min_position, self.max_position, 100)
-            ys = np.ones(100)*screen_height / 2
-            xys = list(zip((xs-self.min_position)*scaler, ys))
-
-            self.track = rendering.make_polyline(xys)
-            self.track.set_linewidth(4)
-            self.viewer.add_geom(self.track)
-
-            for i in range(3):
-                ys = np.ones(100) * upscale * i + upscale / 2
-                xys = list(zip((xs-self.min_position)*scaler, ys))
-
-                zeros = rendering.make_polyline(xys)
-                self.viewer.add_geom(zeros)
-
-            l,r,t,b = -carwidth/2, carwidth/2, carheight, 0
-            car = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-            car.add_attr(rendering.Transform(translation=(0, screen_height/2 - carheight/2)))
-            self.cartrans = rendering.Transform()
-            car.add_attr(self.cartrans)
-            self.viewer.add_geom(car)
-
-        pos = self.state[0]
-        self.cartrans.set_translation((pos-self.min_position)*scaler, 0)
-        
-        if self.t > 1:
-            from gym.envs.classic_control import rendering
-            t = self.t % (screen_width / scaler)
-            for i in range(3):
-                prev, curr = self.prev_render_info[i], self.curr_render_info[i]
-                line = rendering.Line(start=(scaler*(t-1), prev+upscale*i + upscale/2), end=(scaler*t, curr+upscale*i + upscale/2))
-                line.set_color(*COLORS[i])
-                line.linewidth.stroke = 2
-                self.lines.append(line)
-                self.viewer.add_geom(line)
-
-            if t == 0:
-                self.clear_render()
-
-        return self.viewer.render(return_rgb_array = mode=='rgb_array')
-
-    def clear_render(self):
-        BLANK = np.zeros(4)
-        for line in self.lines:
-            line._color.vec4 = BLANK
-        self.lines = []
+        pass
 
     def close(self):
-        if self.viewer: self.viewer.close()
+        pass
